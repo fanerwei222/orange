dc68dfb08d339ef80de99635cc7201a1c0934834
樊伟 <fanerwei222@163.com>
2019-09-04 17:45:59 +0800

    Add files via upload

---
+++ src/main/java/com/domaven/orange/rabbitmq/workqueue/Worker.java
@@ -0,0 +1,53 @@
+package com.domaven.orange.rabbitmq.workqueue;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+/**
+ * TODO
+ * 完成任务的小时工
+ * @author fw
+ * @date 2019/9/4 14:52
+ */
+public class Worker {
+    private final static String TASK_QUEUE_NAME = "task_queue";
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        channel.basicQos(1);
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "utf-8");
+            System.out.println(" [x] Received '" + message + "'");
+            try {
+                doWork(message);
+            } finally {
+                System.out.println(" [x] Done");
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
+        };
+        boolean autoAck = false; // acknowledgment is covered below
+        channel.basicConsume(TASK_QUEUE_NAME, autoAck, deliverCallback, consumerTag -> {});
+    }
+
+    private static void doWork(String task) {
+        for (char ch : task.toCharArray()){
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException  _ignored){
+                    Thread.currentThread().interrupt();
+                }
+
+            }
+        }
+    }
+}
