6faf1ac82391f65de9d839f131a98d697c6551b8
樊伟 <fanerwei222@163.com>
2019-09-04 17:44:18 +0800

    Add files via upload

---
+++ src/main/java/com/domaven/orange/rabbitmq/helloworld/Send.java
@@ -0,0 +1,27 @@
+package com.domaven.orange.rabbitmq.helloworld;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+/**
+ * TODO
+ * 发布者
+ * @author fw
+ * @date 2019/9/4 14:14
+ */
+public class Send {
+    private final static String QUEUE_NAME = "hello";
+    public static void main(String[] args) throws Exception{
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try(Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+        ){
+            channel.queueDeclare(QUEUE_NAME, false, false, false ,null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
