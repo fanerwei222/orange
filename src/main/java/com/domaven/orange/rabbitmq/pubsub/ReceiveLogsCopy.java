2102bcf5e7359d4ad3f7f46b0b2909c43d0d8285
樊伟 <fanerwei222@163.com>
2019-09-04 17:45:07 +0800

    Add files via upload

---
+++ src/main/java/com/domaven/orange/rabbitmq/pubsub/ReceiveLogsCopy.java
@@ -0,0 +1,35 @@
+package com.domaven.orange.rabbitmq.pubsub;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+/**
+ * TODO
+ * 消息接收者副本
+ * @author fw
+ * @date 2019/9/4 17:17
+ */
+public class ReceiveLogsCopy {
+    private static final String EXCHANGE_NAME = "logs";
+    public static void main(String[] args) throws Exception{
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+
+        String queueName = channel.queueDeclare().getQueue();
+        channel.queueBind(queueName, EXCHANGE_NAME, "");
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (cosumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "utf-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {});
+    }
+}
