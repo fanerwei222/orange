fa8d0110e0dbc69b1abbdac80aa1aa7572b9e7fd
樊伟 <fanerwei222@163.com>
2019-09-05 09:27:55 +0800

    Update NewTask.java

--- src/main/java/com/domaven/orange/rabbitmq/workqueue/NewTask.java
+++ src/main/java/com/domaven/orange/rabbitmq/workqueue/NewTask.java
@@ -19,9 +19,18 @@ public class NewTask {
         try(Connection connection = factory.newConnection();
             Channel channel = connection.createChannel();
         ){
+            /**
+             * 是否开启持久化操作
+             */
             boolean durable = true;
+            /**
+             * 声明任务队列
+             */
             channel.queueDeclare(TASK_NAME, durable, false, false ,null);
             String message = "hello... world... this... is .....a ..... new ...task....";
+            /**
+             * 发布消息,消息发布进行持久化操作,便于意外中断时恢复.
+             */
             channel.basicPublish("", TASK_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
             System.out.println(" [x] Sent '" + message + "'");
         }
