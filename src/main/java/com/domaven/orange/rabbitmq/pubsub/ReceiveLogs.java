18ca7a8bd915426f1a40605306a5aa048a7999d4
樊伟 <fanerwei222@163.com>
2019-09-05 09:35:55 +0800

    Update ReceiveLogs.java

--- src/main/java/com/domaven/orange/rabbitmq/pubsub/ReceiveLogs.java
+++ src/main/java/com/domaven/orange/rabbitmq/pubsub/ReceiveLogs.java
@@ -18,18 +18,33 @@ public class ReceiveLogs {
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
+        /**
+         * 声明交换器
+         */
         channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
 
+        /**
+         * 随机获取一个队列名称
+         */
         String queueName = channel.queueDeclare().getQueue();
+        /**
+         * 绑定指定名称的队列
+         */
         channel.queueBind(queueName, EXCHANGE_NAME, "");
 
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
+        /**
+         * 消息处理
+         */
         DeliverCallback deliverCallback = (cosumerTag, delivery) -> {
           String message = new String(delivery.getBody(), "utf-8");
           System.out.println(" [x] Received '" + message + "'");
         };
 
+        /**
+         * 消费消息
+         */
         channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {});
     }
 }
