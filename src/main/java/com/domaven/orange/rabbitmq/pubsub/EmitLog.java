ee5f86612e9d4947631c62a999d431a1ec570adf
樊伟 <fanerwei222@163.com>
2019-09-05 09:34:44 +0800

    Update EmitLog.java

--- src/main/java/com/domaven/orange/rabbitmq/pubsub/EmitLog.java
+++ src/main/java/com/domaven/orange/rabbitmq/pubsub/EmitLog.java
@@ -18,8 +18,14 @@ public class EmitLog {
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel();
         ){
+            /**
+             * 设置交换器名称和模式
+             */
             channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
             String message = args.length < 1 ? "info : hello world" : String.join(" ", args);
+            /**
+             * 发布消息
+             */
             channel.basicPublish(EXCHANGE_NAME, "",null, message.getBytes("utf-8"));
             System.out.println(" [x] Sent '" + message + "'");
         }
